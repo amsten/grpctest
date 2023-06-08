760b346f833bacca8b674b9e19b35fd0243ca751
amund <s.amund@icloud.com>
2023-06-08 22:38:25 +0200

    Created the server

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
+
+	"github.com/amsten/grpctest/invoicer"
+	"google.golang.org/grpc"
 )
 
+type myInvoicerServer struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (s myInvoicerServer) Create(ctx context.Context, in *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte("Hello World"),
+		Docx: []byte("Hello World from docx"),
+	}, nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Could not listen on port 8080: %v", err)
+	}
+	defer lis.Close()
+
+	serverRegistrar := grpc.NewServer()
+	service := &myInvoicerServer{}
+	invoicer.RegisterInvoicerServer(serverRegistrar, service)
+	err = serverRegistrar.Serve(lis)
+	if err != nil {
+		log.Fatalf("Could not serve: %v", err)
+	}
+
 }
